07c5c8ec71c7ee38825f0cc3471546f2ba047fa1
Guillaume Leroi <guillaume.leroi@keyconsulting.fr>
2014-06-25 14:58:43 +0200

    first commit

---
+++ Test/public/js/main.js
@@ -0,0 +1,9 @@
+﻿function init() {
+    var cpt = 0;
+    var elm = document.getElementById( "test" );
+    setInterval( function () {
+        elm.innerText = "BOOO2! " + cpt++;
+    }, 500 );
+}
+
+window.onload = init;
