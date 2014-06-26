b983698d2598651357d7a7e41b1b004b1fe345ab
Guillaume Leroi <guillaume.leroi@keyconsulting.fr>
2014-06-26 14:21:43 +0200

    listen to update

--- Test/public/js/main.js
+++ Test/public/js/main.js
@@ -4,6 +4,25 @@
     setInterval( function () {
         elm.innerText = "BOOO2! " + cpt++;
     }, 500 );
+
+    applicationCache.addEventListener( "updateready", onUpdateReady );
+    applicationCache.addEventListener( "checking", onChecking );
+    applicationCache.addEventListener( "noupdate", onNoUpdate );
+}
+
+function onUpdateReady() {
+    var elt = document.getElementById("update");
+    elt.innerText = "Update ready!";
+}
+
+function onChecking() {
+    var elt = document.getElementById("update");
+    elt.innerText = "Checking!";
+}
+
+function onNoUpdate() {
+    var elt = document.getElementById("update");
+    elt.innerText = "No update!";
 }
 
 window.onload = init;
